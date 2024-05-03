9fad483c6b6b9864259d7ecf30141f9b96986795
Sergey Kirichenko <whole-white-whale@yandex.ru>
2024-05-03 22:26:29 +0300

    Enable sxhkd hotkey daemon

--- home.nix
+++ home.nix
@@ -7,6 +7,22 @@
     username = "sery";
   };
 
+  services.sxhkd = {
+    enable = true;
+
+    keybindings = {
+      "super + Escape" = "pkill -USR1 sxhkd";
+      "super + Return" = "alacritty";
+      "super + w" = "firefox";
+      "super + {_,ctrl + ,shift + }c" = "bspc node {-c,-p cancel,-k}";
+      "super + {_,ctrl + ,shift + }{h,j,k,l}" = "bspc node {-f,-p,-s} {west,south,north,east}";
+      "super + {_,shift + }{1-8}" = "bspc {desktop -f,node -d} '^{1-8}'";
+      "super + ctrl + m" = "bspc desktop -l next";
+      "super + ctrl + {t,p,s,f}" = "bspc node -t {tiled,pseudo_tiled,floating,fullscreen}";
+      "super + ctrl + {x,y,z}" = "bspc node -g {locked,sticky,private}";
+    };
+  };
+
   xsession.windowManager.bspwm = {
     enable = true;
 
