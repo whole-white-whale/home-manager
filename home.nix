dbadb3fbaac933badd7653670c8811268be8a9fa
Sergey Kirichenko <whole-white-whale@yandex.ru>
2024-05-03 21:51:43 +0300

    Enable bspwm window manager

--- home.nix
+++ home.nix
@@ -6,4 +6,26 @@
     stateVersion = "23.11";
     username = "sery";
   };
+
+  xsession.windowManager.bspwm = {
+    enable = true;
+
+    monitors.eDP-1 = [
+      "1"
+      "2"
+      "3"
+      "4"
+      "5"
+      "6"
+      "7"
+      "8"
+    ];
+
+    settings = {
+      border_width = 2;
+      focused_border_color = "#a6e3a1";
+      normal_border_color = "#6c7086";
+      split_ratio = 0.5;
+    };
+  };
 }
