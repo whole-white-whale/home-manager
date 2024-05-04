51402b348e3edf13076fec32f228640292ea58db
Sergey Kirichenko <whole-white-whale@yandex.ru>
2024-05-04 14:47:13 +0300

    Enable Polybar status bar

--- home.nix
+++ home.nix
@@ -46,6 +46,116 @@
   services = {
     picom.enable = true;
 
+    polybar = {
+      enable = true;
+      script = "polybar top &";
+
+      settings = {
+        "bar/top" = {
+          width = "100%:-12px";
+          height = "32px";
+
+          offset-x = "6px";
+          offset-y = "6px";
+
+          background = "#313244";
+          foreground = "#cdd6f4";
+
+          radius = 8;
+          line-size = 2;
+          padding = 1;
+
+          modules-left = "bspwm";
+          modules-center = "date";
+          modules-right = "network pulseaudio battery xkeyboard";
+        };
+
+        "module/bspwm" = {
+          type = "internal/bspwm";
+
+          label-dimmed = "%name%";
+          label-dimmed-padding = 1;
+
+          label-empty = "%name%";
+          label-empty-padding = 1;
+
+          label-focused = "%name%";
+          label-focused-padding = 1;
+          label-focused-underline = "#a6e3a1";
+
+          label-occupied = "%name%";
+          label-occupied-padding = 1;
+          label-occupied-underline = "#89b4fa";
+
+          label-urgent = "%name%";
+          label-urgent-padding = 1;
+          label-urgent-underline = "#f38ba8";
+        };
+
+        "module/date" = {
+          type = "internal/date";
+          date = "%a %b %d %H:%M";
+        };
+
+        "module/network" = {
+          type = "internal/network";
+          interface-type = "wireless";
+
+          label-connected = "%essid%";
+          label-connected-padding = 1;
+          label-connected-underline = "#a6e3a1";
+
+          label-disconnected = "null";
+          label-disconnected-padding = 1;
+          label-disconnected-underline = "#89b4fa";
+        };
+
+        "module/pulseaudio" = {
+          type = "internal/pulseaudio";
+
+          label-muted = "null";
+          label-muted-padding = 1;
+          label-muted-underline = "#89b4fa";
+
+          label-volume = "%percentage:3%%";
+          label-volume-padding = 1;
+          label-volume-underline = "#a6e3a1";
+        };
+
+        "module/battery" = {
+          type = "internal/battery";
+
+          adapter = "ACAD";
+          battery = "BAT1";
+
+          label-charging = "%percentage:3%%";
+          label-charging-padding = 1;
+          label-charging-underline = "#a6e3a1";
+
+          label-discharging = "%percentage:3%%";
+          label-discharging-padding = 1;
+          label-discharging-underline = "#89b4fa";
+
+          label-full = "%percentage:3%%";
+          label-full-padding = 1;
+          label-full-underline = "#f9e2af";
+
+          label-low = "%percentage:3%%";
+          label-low-padding = 1;
+          label-low-underline = "#f38ba8";
+        };
+
+        "module/xkeyboard" = {
+          type = "internal/xkeyboard";
+          label-indicator-on = "";
+
+          label-layout = "%layout%";
+          label-layout-padding = 1;
+          label-layout-underline = "#89b4fa";
+        };
+      };
+    };
+
     sxhkd = {
       enable = true;
 
